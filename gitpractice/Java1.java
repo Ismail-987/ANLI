853b887a447b86c7f49b45c15a7e3347947cacdd
Ismail-987 <159584108+Ismail-987@users.noreply.github.com>
2024-09-26 12:52:47 +0800

    Version 2
    This is the second version

---
+++ gitpractice/Java1.java
@@ -0,0 +1,7 @@
+public class Java1 {
+    public static void main (String [] args){
+        System.out.println("Welcome to the world of Java and do not fear as ALLAH is with you.");
+        System.out.print("Know that this life is nothing but a test.");
+        System.err.println("This is an not an error sorry and welcome again.");
+    }
+}
